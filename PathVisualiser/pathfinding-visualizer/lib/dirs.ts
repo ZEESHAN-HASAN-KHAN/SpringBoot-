ed20429fcd02e2dc101e3e4f1b378cc49cabcf35
Zeeshan Hasan Khan <91018365+ZEESHAN-HASAN-KHAN@users.noreply.github.com>
2025-05-14 00:08:02 +0530

    Delete PathVisualiser/pathfinding-visualizer directory

--- PathVisualiser/pathfinding-visualizer/lib/dirs.ts
+++
@@ -1,7 +0,0 @@
-import { Coords } from "./types";
-
-export const DIRS: Coords[] = [
-    [1, 0],  [-1, 0],
-    [0, 1],  [0, -1],
-  ];
-  
--- PathVisualiser/pathfinding-visualizer/lib/types.ts
+++
@@ -1,6 +0,0 @@
- export type Grid = number[][];           // 0 empty ·1 wall ·2 weight
-export type Coords = [number, number];       // row, col
-export interface RunResult {
-  visited: Coords[];
-  path: Coords[];
-}
